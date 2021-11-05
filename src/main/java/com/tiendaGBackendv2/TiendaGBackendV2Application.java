9f516872219b5ff3c55d713ec6f7ce3a3aae8f7f
EduaRuiz <edwar_775@hotmail.com>
2021-11-04 22:51:00 -0500

    primer commit con datos basicos

---
+++ src/main/java/com/tiendaGBackendv2/TiendaGBackendV2Application.java
@@ -0,0 +1,13 @@
+package com.tiendaGBackendv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaGBackendV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaGBackendV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendaGBackendv2/TiendaGBackendV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaGBackendv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaGBackendV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
